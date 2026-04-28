51433e2fdfa63e51c1a4e2b22d8501cf2a15102a
Sanskruti_Agasgekar <sanskruti_agasgekar@Sanskruti-Agasgekars-MacBook-Air.local>
2026-04-28 23:28:35 +0530

    lec 24

---
+++ day8/iife.js
@@ -0,0 +1,7 @@
+(function chai() {
+    console.log('DB connected') ;
+})() // this is an IIFE (Immediately Invoked Function Expression), it is a function that is defined and immediately invoked, it is used to create a new scope and avoid polluting the global scope, it is also used to execute some code immediately without having to call the function separately.
+
+((name) => {
+    console.log(`Hello, ${name}!`);
+})('John');
